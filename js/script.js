b6385881ac9f5af358b2bf8cf7f552910f2d224d
sijonx <akmalrpl1@gmail.com>
2023-08-05 07:51:56 -0400

    blah

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function openNav() {
+    document.getElementById("sidebar").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "200px";
+}
+
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+document.getElementById('theme-button').addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme');
+});
